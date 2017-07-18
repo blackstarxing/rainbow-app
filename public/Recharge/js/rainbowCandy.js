40982e5d914df73f03816afccb7ca299be8946a6
zhaoxiaofan <18301545298@163.com>
2017-07-18 16:08:57 +0800

    update

--- public/Recharge/js/rainbowCandy.js
+++ public/Recharge/js/rainbowCandy.js
@@ -116,45 +116,55 @@ var rainbowCandy = new Vue({
                 },
                 success: function(data) {
                   if(data.code == 0){
-                  	var weixinPay = data.object;
-                  	var appId = weixinPay.appId;
-                  	var nonce_str = weixinPay.nonce_str;
-                  	var package = weixinPay.package;
-                  	var timeStamp = weixinPay.timeStamp;
-                  	var paySign = weixinPay.paySign;
-                  	var signType = weixinPay.signType; 
-                  	function onBridgeReady(){
-						WeixinJSBridge.invoke(
-						    'getBrandWCPayRequest', {
-						        "appId":appId,     //公众号名称，由商户传入     
-						        "timeStamp":timeStamp,         //时间戳，自1970年以来的秒数     
-						        "nonceStr":nonce_str, //随机串     
-						        "package":package,     
-						        "signType":signType,         //微信签名方式：     
-						        "paySign":paySign //微信签名 
-						    },
-							function(res){     
-							    if(res.err_msg == "get_brand_wcpay_request:ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-							}
-						); 
-					}
-					if (typeof WeixinJSBridge == "undefined"){
-					   if( document.addEventListener ){
-					       document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-					   }else if (document.attachEvent){
-					       document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
-					       document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-					   }
-					}else{
-						onBridgeReady();
-					}
-                  }else{
-                    layer.open({
-                      content: '服务器出错',
-                      btn: '好的',
-                      shadeClose: false,
-                    });
-                  }
+	                  	var weixinPay = data.object;
+	                  	var appId = weixinPay.appId;
+	                  	var nonce_str = weixinPay.nonce_str;
+	                  	var package = weixinPay.package;
+	                  	var timeStamp = weixinPay.timeStamp;
+	                  	var paySign = weixinPay.paySign;
+	                  	var signType = weixinPay.signType; 
+	                  	callpay();
+		                function onBridgeReady(){  
+						    WeixinJSBridge.invoke(  
+						        'getBrandWCPayRequest', {  
+						             "appId":appId,     //公众号名称，由商户传入  
+						             "paySign":paySign,         //微信签名  
+						             "timeStamp":timeStamp, //时间戳，自1970年以来的秒数  
+						             "nonceStr":nonceStr , //随机串  
+						             "package":packageStr,  //预支付交易会话标识  
+						             "signType":signType     //微信签名方式  
+						         },  
+						         function(res){  
+						          if(res.err_msg == "get_brand_wcpay_request:ok" ) {  
+						         //window.location.replace("index.html");  
+						         alert('支付成功');  
+						     }else if(res.err_msg == "get_brand_wcpay_request:cancel"){  
+						         alert('支付取消');  
+						     }else if(res.err_msg == "get_brand_wcpay_request:fail" ){  
+						        alert('支付失败');  
+						     } //使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。  
+						         }  
+						    );  
+						 };  
+						function callpay(){  
+						    if (typeof WeixinJSBridge == "undefined"){  
+						        if( document.addEventListener ){  
+						            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);  
+						        }else if (document.attachEvent){  
+						            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);  
+						            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);  
+						        }  
+						    }else{  
+						        onBridgeReady();  
+						    }  
+						}
+	                }else{
+	                  layer.open({
+	                    content: '服务器出错',
+	                    btn: '好的',
+	                    shadeClose: false,
+	                  });
+	                }
                 },  
                 error: function() {
                     layer.open({
@@ -165,7 +175,10 @@ var rainbowCandy = new Vue({
                 }
             });
 
-        }
+        },
+
+        // 微信支付
+        
     }
 })
 	
