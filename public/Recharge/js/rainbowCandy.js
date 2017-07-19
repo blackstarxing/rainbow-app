c76be672fc31ed050961137cf8a0dd878458f325
zhaoxiaofan <18301545298@163.com>
2017-07-19 16:45:49 +0800

    update

--- public/Recharge/js/rainbowCandy.js
+++ public/Recharge/js/rainbowCandy.js
@@ -133,26 +133,25 @@
 						             "signType":signType     //微信签名方式  
 						         },  
 						         function(res){  
+                                    alert('返回'+res);
 						          	if(res.err_msg == "get_brand_wcpay_request:ok" ) {  
 						           		 var _this = this;
-								        _this.payPrompt = true; 
 								        _this.payText = '支付成功';
 								         setTimeout(function(){
-								        	_this.payPrompt = false; 
+								        	_this.payPrompt = true; 
 								        },2000); 
 								    }else if(res.err_msg == "get_brand_wcpay_request:cancel"){  
 								        var _this = this;
-								        _this.payPrompt = true;  
 								        _this.payText = '支付失败';
 								        setTimeout(function(){
-								        	_this.payPrompt = false; 
+								        	_this.payPrompt = true;  
 								        },2000);
 								    }else if(res.err_msg == "get_brand_wcpay_request:fail" ){  
 								         var _this = this;
 								        _this.payPrompt = true; 
 								        _this.payText = '支付失败'; 
 								         setTimeout(function(){
-								        	_this.payPrompt = false; 
+								           _this.payPrompt = true; 
 								        },2000);  
 								    } //使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。  
 						         }  
